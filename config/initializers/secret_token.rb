939afff29a6137c78ba532928c1ff5fe45de4fad
Sabarish Kumar R <skumar@crri.co.in>
2014-08-06 01:08:50 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RssReader::Application.config.secret_key_base = '800ac5215b2b55c7099703b63ef1b73c59fbe90589f7c3f0aeb0a54048009ae88ebf8610ffcdf077a4590216b996521808fd8510c956eab982f401e48484cd1d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RssReader::Application.config.session_store :cookie_store, key: '_rss_reader_session'
