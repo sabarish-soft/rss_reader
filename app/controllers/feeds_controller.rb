cdca9f36892b5f138b7f7dfa3833dec6416b3519
Sabarish Kumar R <skumar@crri.co.in>
2014-08-06 03:01:59 +0530

    Unified feeds page and auto updating feed title

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,7 +1,8 @@
 class FeedsController < ApplicationController
   def list
   	feed_urls = FeedSource.pluck(:url)
-    feed = Feedjira::Feed.fetch_and_parse(feed_urls)
-    @entry = feed.entries
+    feeds = Feedjira::Feed.fetch_and_parse(feed_urls)
+    #Fetch all feed and sort in reverse chronological order
+    @entry = feeds.entries.flat_map(&:last).flat_map(&:entries).sort{|p,n| n.published <=> p.published}
   end
 end
--- app/models/feed_source.rb
+++ app/models/feed_source.rb
@@ -1,2 +1,14 @@
 class FeedSource < ActiveRecord::Base
+  before_save :validate_and_update_source
+
+  def validate_and_update_source
+    begin
+    feed = Feedjira::Feed.fetch_and_parse(url)
+    raise 'URL not valid' unless feed.respond_to?(:title)
+    self.name = feed.title
+    rescue => e
+      self.errors.add(:base, "Invalid Feed - #{e.to_s}")
+      return false
+    end
+  end
 end
